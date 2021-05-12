0e8032759e21fce00ab148cf2c12795c14fbb2c8
Realman23 <62398192+Realman23@users.noreply.github.com>
2021-05-12 17:40:49 +0300

    Initial commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,97 @@
+import React from 'react';
+
+function HomePage(){
+    return(
+        <>
+            <h1>Home:</h1>
+            <h1>Welcome to the our trip to the Latgale!</h1>
+            <h2>We are glad to see you there</h2>
+
+            <h1>Price:</h1>
+            <p>Have you ever rode canoes along the Daugava, or have you been in the Daugavpils fortress? No? Then this tour is for you! Traveling with us, you will see all the sights of the city, as well as their historical monuments. </p>
+            <p>Price: This absorbing journey will include in the price a hotel(with breakfast and dinner), canoe,ticket to the Daugavpils fortress with guide and all possible entertainments.It will cost 80 euro per person. </p>
+
+            <h1>Schedule:</h1>
+
+            <h1>About Us:</h1>
+            <p>We are travel company from Latvia, Riga, who gives you an unforgettable journey to the Latgale.
+                Our contacts:
+                sdasdas
+                das
+                das
+                da
+                s
+
+                <h1>Travel with us and have fun!</h1>
+            </p>
+
+        </>
+    )
+}
+// nav {
+//     display: flex;
+//     align-items: center;
+//     min-height: 8vh;
+//     background-color: white;
+//     font-family: "Poppins", sans-serif;
+//     width: 99%;
+//     box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+//     border-radius: 15px;
+//     margin: 5px auto 0 auto;
+// }
+//
+// ul {
+//     display: flex;
+//     justify-content: space-around;
+//     width: 30%;
+//     position: absolute;
+//     right: 50px;
+// }
+//
+// li {
+//     list-style: none;
+//     cursor: pointer;
+//     text-decoration: none;
+//     font-size: 20px;
+//     font-family: 'Bubblegum Sans', cursive;
+//     opacity: 0.7;
+// }
+//
+// .logo {
+//     text-transform: uppercase;
+//     letter-spacing: 3px;
+//     font-size: 22px;
+//     margin-left: 50px;
+//     font-family: 'Allerta Stencil', sans-serif;
+//     display: flex;
+//     cursor: pointer;
+// }
+// .logo >h1{
+//     padding-top: 7px;
+//     padding-left:15px;
+// }
+// img{
+//     height: 55px;
+//     width: 55px;
+// }
+// .cool-link::after {
+//     content: '';
+//     display: block;
+//     width: 0;
+//     height: 3px;
+//     background: linear-gradient(315deg, #ff4e00 0%, #ec9f05 74%);
+//     border-radius: 15px;
+//     transition: width .3s;
+// }
+//
+// .cool-link:hover::after {
+//     width: 100%;
+//     opacity: 1;
+//     transition: width .3s;
+// }
+//
+// #active {
+//     border-bottom: 3px solid #ffa800;
+//     opacity: 1;
+// }
+export default HomePage;
